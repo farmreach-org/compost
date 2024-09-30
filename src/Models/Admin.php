f157dd7bf6786352600a34891e3eaefb217cc3d2
nidheeshdas <nidheeshdas@gmail.com>
2024-09-30 18:30:06 +0530

    added workspace setup for new company admins

--- src/Models/Admin.php
+++ src/Models/Admin.php
@@ -7,6 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Inovector\Mixpost\Concerns\UsesUserModel;
 
+/**
+ * Inovector\Mixpost\Models\Admin
+ *
+ * @property int $id
+ * @property string $user_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\User $user
+ * @method static \Inovector\Mixpost\Database\Factories\AdminFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder|Admin newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Admin newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Admin query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Admin whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Admin whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Admin whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Admin whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Admin extends Model
 {
     use HasFactory;
